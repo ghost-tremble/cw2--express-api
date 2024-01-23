22c8b9cda3650b7e792b850b30c63114c393a7fd
ghost-tremble <=>
2024-01-23 19:16:23 +0000

    middleware update

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ const lessonsRouter = require('./routes/lessonRoutes')
 const ordersRouter = require('./routes/orderRoutes');
 const searchRouter = require('./routes/searchRoutes');
 const logger = require('./middleware/logger');
-const staticFile = require('./middleware/staticFile');
+
 
 
 
 
 //middleware
 app.use(logger)
-app.use('/images', staticFile )
+app.use('/images', async (req, res, next) => {
+    const filePath = path.join(__dirname, "static", req.url, "")
+    
+    fs.stat(filePath, (err, fileInfo) => {
+        if (err) return res.status(404).send({message: "File not found"});
+        if (fileInfo.isFile()) res.sendFile(filePath);
+        else next()
+    })
+})
+
 app.use(express.json())
 app.use(cors())
 
